b6dcc6f450f15503a06ef819a85f3b13b9b5a40f
JesusOyola <jesus.oyola@interatica.com>
2023-07-22 04:47:32 -0300

    Se creó el layout del dashboard y si implementó el servicio para obtener los productos

---
+++ src/app/interface/products.ts
@@ -0,0 +1,14 @@
+import { ProductImages } from './products-images';
+
+export interface Products {
+  destacado: number;
+  nombre: string;
+  id_producto: number;
+  id_subcategoria: number;
+  precio: number;
+  imagenes: [ProductImages];
+  vendible: number;
+  stock: number;
+  garantia: number;
+  iva: number;
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from 'src/app/interface/products';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  listProducts:Products;
 
-  constructor() { }
+  constructor(private readonly productService:ProductService) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts(){
+    this.productService.getProducts().subscribe({
+      next:(data)=>{
+        console.log(data)
+         this.listProducts = data; 
+      }
+    })
   }
 
 }
+
+
+/* Para las imágenes la url es esta
+a la documentación le falta explicar que sin compragamer_Imganen_general_ no funciona.
+`https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_${nombre de la imagen}`
+
+*/
